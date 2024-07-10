03836222ad161a0718f0291434e533944294eb96
danilobml <danbarml@gmail.com>
2024-07-10 14:49:10 +0200

    v 0.1.8 - add:list_get; modify: readme and setup

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ A package for common use tools by the Dev team at GP Joule Connect.
 ## Tools:
 - exectime: decorator to measure the execution time of a function.
 
+- list_get: similar to dict.get(), but for lists: returns the element at index or None.
+
 ## Installation:
 
 ```bash
--- gpctools_package/setup.py
+++ gpctools_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpctools",
-    version="0.1.6",
+    version="0.1.8",
     author="Danilo B. M. de Lima",
     author_email="d.lima@gp-joule.de",
     description="Python package for common use tools",
@@ -15,5 +15,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.11',
 )
